9fb2a607850787d62a26fb930a503f6c2d1ba13d
unknown <sakethp1998@gmail.com>
2021-08-07 14:16:21 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
+//https://www.youtube.com/watch?v=w7ejDZ8SWv8
+//sourceytlink
+//rafce
+const App = () =>
+{
+
+  const [showAddTask,setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+  useEffect(()=>{})
+
+//Add Task
+const addTask = (task) =>
+{
+    const id = Math.floor(Math.random() * 10000) + 1
+    
+    const newTask   = { id,  ...task }
+    setTasks([...tasks, newTask])
+}
+
+//Delete task
+const deleteTask = (id) =>
+{
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+
+// Toggle Remainder
+const toggleReminder = (id) =>
+{
+  setTasks(tasks.map((task)=> task.id === id 
+  ? { ...task,reminder: !task.reminder} : task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={ ()=> setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd= {addTask}/>}
+      {tasks.length > 0 ? (<Tasks  tasks={tasks} onDelete={deleteTask} 
+      onToggle={toggleReminder}/>) : ("No tasks to Show")}
     </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'; //impt
+import Button from './Button'
+
+
+//rafce
+const Header = ({ title, onAdd, showAdd }) => {
+   
+
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+        </header>
+    )
+}
+Header.defaultProps = {
+    title : "Task tracker",
+}
+
+Header.propTypes = {
+    title : PropTypes.string.isRequired,
+}
+
+//CSS in JS
+// const headingStyle = {
+//     color: "red", 
+//     backgroundColor:"black"
+// }
+
+export default Header
